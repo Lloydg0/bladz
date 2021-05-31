8dad66a042064f1050687ed111deb9a62940a0f2
Lloyd Grogan <lloydgrogan@hotmail.com>
2021-05-31 22:57:18 +0200

    fixed widget dashboard

--- client/src/charts.js
+++ client/src/charts.js
@@ -44,7 +44,7 @@ export default function Charts() {
                 </div>
             </div>
             <TechAnalysis />
-            {/* <MarketOverView /> */}
+            <MarketOverView />
         </>
     );
 }
